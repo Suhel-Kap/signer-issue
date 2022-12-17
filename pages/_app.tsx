75f312eb61c85f1d72d77a945837325b32b7e300
Suhel Kapadia <suhelkapadia2@gmail.com>
2022-12-18 02:17:19 +0530

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,53 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import {AppProps} from 'next/app';
+import Head from 'next/head';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme
+} from '@rainbow-me/rainbowkit';
+import {
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import {alchemyProvider} from 'wagmi/providers/alchemy';
+import {publicProvider} from 'wagmi/providers/public';
+import {useState, useEffect} from "react";
+import {polygonMumbai} from "@wagmi/chains";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const [user, setUser] = useState(null);
+  const {Component, pageProps} = props;
+  const {chains, provider, webSocketProvider} = configureChains(
+      [polygonMumbai],
+      [
+        alchemyProvider({apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY!}),
+        publicProvider()
+      ]
+  );
+  const {connectors} = getDefaultWallets({
+    appName: 'Dynamic Audio NFTs',
+    chains
+  });
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+    webSocketProvider
+  })
+
+
+  return (
+      <>
+        <Head>
+          <title>The Crypto Studio</title>
+        </Head>
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider chains={chains} theme={darkTheme()}>
+                  <Component {...pageProps} />
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </>
+  );
 }
