96b70c8c8dbb6cada3e697aab9929c511b9f8b09
Ayush Bhadauria <72291803+ayushh007@users.noreply.github.com>
2022-12-18 12:38:51 +0530

    Transit Update
    useSigner hook is not being called at the right time. The useSigner hook is a reactive hook, which means that it will only update the value of signer when the component re-renders. This means that if the component is not re-rendering, the value of signer will not be updated.
    
    i've tried adding a state variable id and using the useEffect hook to update its value when the query parameter changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
 import {ConnectButton} from "@rainbow-me/rainbowkit";
 import {useSigner} from "wagmi";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  const {data: signer} = useSigner()
-  console.log("signer", signer)
+  const router = useRouter();
+  const [id, setId] = useState(router.query.id);
+  const { data: signer } = useSigner();
+
+  useEffect(() => {
+    setId(router.query.id);
+  }, [router.query.id]);
+
+  console.log("signer", signer);
 
   return (
-      <>
-        <ConnectButton />
-      </>
-  )
+    <>
+      <ConnectButton />
+    </>
+  );
 }
